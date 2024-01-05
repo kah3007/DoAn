fd1c2798b63dc8399992b242d78c9920ce9984d0
kah3007 <144111252+kah3007@users.noreply.github.com>
2024-01-05 20:05:30 +0700

    Update delete-comment.php

--- DoAnCoSo2/Controller/article-action/delete-comment.php
+++ DoAnCoSo2/Controller/article-action/delete-comment.php
@@ -1,7 +1,9 @@
 <?php
     include_once __DIR__ . '/../../model/database.php';
     include_once __DIR__ . '/../../model/comment.php';
-
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
     if (isset($_GET['commentId']) && isset($_GET['articleId'])) {
         $commentId = filter_var($_GET['commentId'], FILTER_SANITIZE_NUMBER_INT);
         $articleId = filter_var($_GET['articleId'], FILTER_SANITIZE_NUMBER_INT);
@@ -10,7 +12,12 @@
         $comment = new Comment($db);
         $result = $comment->deleteComment($commentId);
         if ($result) {
-            header("Location: ../../View/user/article-view.php?id=".$articleId);
+            if($_SESSION['role'] == 0){
+                header("Location: ../../View/user/article-view.php?id=".$articleId);
+            }
+            else{
+                header("Location: ../../View/admin/article-view.php?id=".$articleId);
+            }
             exit();
         } else {
             echo "Error deleting user.";
